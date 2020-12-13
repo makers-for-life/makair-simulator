285fec84899062af63d3a9e74c34ac2421acf1d6
ElieCo <elieco96@gmail.com>
2020-12-13 22:27:35 +0100

    don't call pipenv in the script
    (everyone manage their own python env as they want)

--- build/build_launch_postprocess.sh
+++ build/build_launch_postprocess.sh
@@ -10,6 +10,4 @@ printf "\nrunning the exectutable...\n"
 
 echo "lauching postprocess..."
 cd ../data
-#opening venv
-source simulator_venv/bin/activate
 python3 display_experiment_results.py simulation_output/log_simulator.csv 
