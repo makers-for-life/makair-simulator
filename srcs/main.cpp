963c053c676ed87439627e61386b91de89d64249
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-01-03 20:54:39 +0100

    provide serial port as arg

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,7 +11,24 @@
 #include <iostream>
 
 // Main function
-int main(void) {
+int main(int argc, char* argv[]) {
+
+    cout << "Welcome to :" << argv[0] << endl;
+    if (argc == 3 && argv[1] == "-p") {
+        Serial6 = SerialFake("/dev/pts/3");
+    } else if (argc == 3 && argv[1] == "--help") {
+        Serial6 = SerialFake("/dev/pts/3");
+        cout << "Usage : makair-simulator -p [PORT NAME]" << endl;
+        cout << "Example : makair-simulator -p /dev/ttyACM0" << endl;
+        return 0;
+    } else if (argc == 1) {
+        Serial6 = SerialFake(argv[2]);
+    } else {
+        cout << "Wrong usage" << endl;
+        cout << "Usage : makair-simulator -p [PORT NAME]" << endl;
+        cout << "Example : makair-simulator -p /dev/ttyACM0" << endl;
+        return 1;
+    }
 
     Serial6 = SerialFake("/dev/pts/3");
     Simulator simulator;
