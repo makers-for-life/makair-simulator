fe18b4f80cbfad343c457726eef32065823310f2
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-01-03 21:01:25 +0100

    fix

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 
     cout << "Welcome to Makair simulator" << endl;
     if (argc == 3 && string(argv[1]) == "-p") {
-        Serial6 = SerialFake("/dev/pts/3");
+        Serial6 = SerialFake(argv[1]);
     } else if (argc == 2 && argv[1] == "--help") {
         Serial6 = SerialFake("/dev/pts/3");
         cout << "Usage : makair-simulator -p [PORT NAME]" << endl;
