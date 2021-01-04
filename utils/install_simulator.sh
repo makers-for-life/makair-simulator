e6a7a60a883362055c162ecc14518df3164d79fa
Benhalor <gabriel.moneyron@yahoo.fr>
2021-01-04 23:05:41 +0100

    Update install_simulator.sh

--- utils/install_simulator.sh
+++ utils/install_simulator.sh
@@ -2,13 +2,17 @@
 
 git clone https://github.com/makers-for-life/makair-simulator --recurse-submodules
 git --git-dir makair-simulator/.git --work-tree=makair-simulator checkout test-with-ui
+git --git-dir makair-simulator/.git --work-tree=makair-simulator pull
 git --git-dir makair-simulator/makair-firmware/.git --work-tree=makair-simulator/makair-firmware/ checkout dev
+git --git-dir makair-simulator/makair-firmware/.git --work-tree=makair-simulator/makair-firmware/ pull
 make -C makair-simulator/build
 
 git clone https://github.com/makers-for-life/makair-control-ui
+git --git-dir makair-control-ui/.git --work-tree=makair-control-ui/ pull
 cargo build --manifest-path=makair-control-ui/Cargo.toml
-mkdir dev
-socat -d -d pty,link=dev/tty0,raw,echo=0 pty,link=dev/tty1,raw,echo=0 & pid="$!"
+mkdir serialport
+socat -d -d pty,link=serialport/tty0,raw,echo=0 pty,link=serialport/tty1,raw,echo=0 & pid="$!"
 wait pid
-./makair-simulator/build/simulator -p dev/tty0 &
-./makair-control-ui/target/debug/makair-control -p dev/tty1
+sleep 1
+./makair-simulator/build/simulator -p serialport/tty0 &
+./makair-control-ui/target/debug/makair-control -p serialport/tty1
