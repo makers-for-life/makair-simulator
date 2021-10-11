33cc91b3ade4cf7b7c6d3770c5be6fa683a6febd
Benhalor <gabriel.moneyron@yahoo.fr>
2021-10-11 20:59:29 +0200

    freeze makair-control-ui dependecy

--- utils/install_simulator.sh
+++ utils/install_simulator.sh
@@ -5,9 +5,12 @@ git --git-dir makair-simulator/.git --work-tree=makair-simulator checkout 1e9539
 git --git-dir makair-simulator/.git --work-tree=makair-simulator pull
 git --git-dir makair-simulator/makair-firmware/.git --work-tree=makair-simulator/makair-firmware/ checkout 68967cf
 git --git-dir makair-simulator/makair-firmware/.git --work-tree=makair-simulator/makair-firmware/ pull
+sed -i '58i#define SIMULATOR' makair-simulator/makair-firmware/includes/config.h
+sed -i '59d' makair-simulator/makair-firmware/includes/config.h
 make -C makair-simulator/build
 
 git clone https://github.com/makers-for-life/makair-control-ui
+git --git-dir makair-control-ui/.git --work-tree=makair-control-ui checkout c29bad2
 git --git-dir makair-control-ui/.git --work-tree=makair-control-ui/ pull
 cargo build --manifest-path=makair-control-ui/Cargo.toml
 mkdir serialport
