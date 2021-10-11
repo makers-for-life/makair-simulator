0ac14e15112c2b556b0ac06981c2f8f1bd9cc65a
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-10-11 20:47:09 +0200

    remove deprecated utilities

--- build/docker.sh
+++
@@ -1,6 +0,0 @@
-socat PTY,link=/dev/ttyS10 PTY,link=/dev/ttyS11 &
-socat tcp-l:7777,reuseaddr,fork file:/dev/ttyS11,nonblock,raw,echo=0,waitlock=/srv/tty.lock &
-
-sleep 2
-
-/srv/simulator/build/simulator -p /dev/ttyS11
