619e24bcc2dc590e7b47704302364bb295c84f03
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-10-24 13:15:20 +0200

    add a file to fix linking problem when compiling to wasm through the rust toolchain

---
+++ srcs/gxx_personality_v0_stub.cpp
@@ -0,0 +1,17 @@
+#ifdef SIMULATOR_WASM
+
+// This file is a "hack" to fix a linking problem when compiling for wasm through rust. see
+// https://stackoverflow.com/questions/67474533/error-in-compiling-rust-into-webassembly-using-emscripten-on-windows
+// and https://github.com/rust-lang/rust/issues/85821
+#include "stdint.h"
+#include "unwind.h"
+
+extern "C" _Unwind_Reason_Code __gxx_personality_v0(int version,
+                                                    _Unwind_Action actions,
+                                                    uint64_t exceptionClass,
+                                                    _Unwind_Exception* unwind_exception,
+                                                    _Unwind_Context* context) {
+    return _URC_NO_REASON;
+}
+
+#endif
