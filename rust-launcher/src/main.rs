da6bbb146a9347847092e5a28def5cd3d121a999
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2021-11-06 11:12:17 +0100

    add a second thread

--- rust-launcher/src/main.rs
+++ rust-launcher/src/main.rs
@@ -1,6 +1,7 @@
 extern crate libc;
 use std::thread;
 use std::os::raw;
+use std::time;
 
 extern {
     fn serialBufferSize() -> raw::c_int;
@@ -25,8 +26,16 @@ fn main() {
         let size_of_buffer = unsafe{serialBufferSize()};
         println!("Size of TX buffer = {}",size_of_buffer);
         println!("Value of TX buffer index = {}",unsafe{*getTXSerialBufferIndexPointer()});
-        for i in 0..size_of_buffer {
-            println!("TX buffer[{}] = {}",i, unsafe{ *getTXSerialBufferPointer().offset(i as isize) });
+
+        // Display content of the buffer
+        loop{
+            print!("Content of TX bufffer: ");
+            for i in 0..size_of_buffer {
+                print!("{} ", unsafe{ *getTXSerialBufferPointer().offset(i as isize) });
+            }
+            println!();
+            thread::sleep(time::Duration::from_millis(2000));
+
         }
     });
 
